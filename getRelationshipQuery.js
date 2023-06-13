ab3a19234fd3a26b2150693fda3b836185bea404
Suman Kundu <devsumanmdn@gmail.com>
2023-06-13 21:02:38 +0530

    make relationship query work

--- getRelationshipQuery.js
+++ getRelationshipQuery.js
@@ -1,5 +1,5 @@
 const getRelationshipQuery = (label1, label2, attribute1, attribute2, relationshipName) =>{
-  return `match (a: ${label1}), (b:${label2}) where a.${attribute1} = b.${attribute2} create (a)-(r:${relationshipName})->(b)`;
+  return `match (a: ${label1}), (b:${label2}) where a.${attribute1} = b.${attribute2} create (a)-[r:${relationshipName}]->(b)`;
 }
 
 module.exports = getRelationshipQuery;
