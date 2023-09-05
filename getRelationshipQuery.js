b7e75f7ec4b28bf4558b653e23569234fe8f75e3
BhaswatiBarma <121888514+BhaswatiBarma@users.noreply.github.com>
2023-09-05 10:04:53 +0530

    fix queries

--- getRelationshipQuery.js
+++ getRelationshipQuery.js
@@ -1,13 +1,5 @@
 const getRelationshipQuery = (label1, label2, attribute1, attribute2, relationshipName) =>{
-  return `match
-    (a: ${label1}), (b:${label2})
-    (
-      with a,b
-      where a.${attribute1} = b.${attribute2}
-    create (a)-[r:${relationshipName}]->(b)
-    )
-    IN TRANSACTIONS OF 10000 ROWS
-    `;
+  return `match (a: ${label1}), (b:${label2}) where a.${attribute1} = b.${attribute2} create (a)-[r:${relationshipName}]->(b)`;
 }
 
 module.exports = getRelationshipQuery;
